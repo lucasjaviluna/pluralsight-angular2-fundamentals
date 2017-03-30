688c224d5824af5887555efaf6a21e54bbc33a2d
lucas <lucasjaviluna@gmail.com>
2017-03-30 13:07:59 -0300

    Using multiple Validators in Reactive Forms

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -24,8 +24,8 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.firstName = new FormControl(this.authService.currentUser.firstName, Validators.required);
-    this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required);
+    this.firstName = new FormControl(this.authService.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')]);
+    this.lastName = new FormControl(this.authService.currentUser.lastName, [Validators.required]);
     this.profileForm = new FormGroup({
       firstName: this.firstName,
       lastName: this.lastName
