3946c3b398feeb2ad11fbb253aa4a9195a164554
lucas <lucasjaviluna@gmail.com>
2017-03-30 11:44:31 -0300

    Creating your first Reactive Form

--- app/user/user.module.ts
+++ app/user/user.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {userRoutes} from './user.routes';
 import {ProfileComponent} from './profile.component';
@@ -12,6 +12,7 @@ import {LoginComponent} from './login.component';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(userRoutes)
     //definimos las rutas como forChild a diferencia de forRoot como en el modulo principal de la app
   ],
