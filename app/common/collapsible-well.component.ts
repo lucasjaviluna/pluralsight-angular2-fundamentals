8a5fd2bf820fb2b587b37f3d8311271e939a50d2
lucas <lucasjaviluna@gmail.com>
2017-04-04 14:24:56 -0300

    Multiple slot Content Projection

--- app/common/collapsible-well.component.ts
+++ app/common/collapsible-well.component.ts
@@ -4,11 +4,19 @@ import {Component, Input} from '@angular/core';
   selector: 'collapsible-well',
   template: `
     <div (click)="toggleContent()" class="well pointable">
-      <h4 class="well-title">{{title}}</h4>
-      <ng-content *ngIf="visible"></ng-content>
+      <h4>
+        // Aca va el title
+        <ng-content select="[well-title]"></ng-content>
+      </h4>
+      // Aca va el body
+      <ng-content *ngIf="visible" select="[well-body]"></ng-content>
     </div>
   `
 })
+
+//select=".title"  es cuando pongo una class="title" en el bloque se código hijo que se va a cargar
+//select=""[atributo]"" lo mismo pero con un atributo. Es mejor
+
 export class CollapsibleWellComponent {
   @Input() title:string;
   visible: boolean = true;
